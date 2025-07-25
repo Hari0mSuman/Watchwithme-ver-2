e55742a067d5971cfddba3b2e188c6be2a1df3b1
Hariom Suman <635494hs@gmail.com>
2025-07-25 22:04:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from sqlalchemy.orm import DeclarativeBase
 from werkzeug.middleware.proxy_fix import ProxyFix
-
+import routes
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
